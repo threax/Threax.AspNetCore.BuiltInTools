e9397886e4586b7f5d18b6636bff9534ac74ec13
Andrew Piper <andrew@threax.com>
2022-03-13 12:59:14 -0400

    Update to net6.0.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.AspNetCore.BuiltInTools.git
+git push https://github.com/threax/Threax.AspNetCore.BuiltInTools.git --tags
+git push https://gitlab.com/threax-software/Threax.AspNetCore.BuiltInTools.git
+git push https://gitlab.com/threax-software/Threax.AspNetCore.BuiltInTools.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.BuiltInTools
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.BuiltInTools --tags
